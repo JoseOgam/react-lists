698c358fa54a12966a88d8fd23a7dc11bbad0f18
JoseOgam <jotienoochieng@gmail.com>
2020-07-24 12:36:38 -0400

    intergrating font-awesome in react-app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import Header from './navigation/header'
+import React from "react";
+import Header from "./navigation/header";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithubAlt,
+  faGoogle,
+  faFacebook,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function App () {
+function App() {
   return (
     <div>
       <Header />
+      <FontAwesomeIcon icon={faHome} />
+      <FontAwesomeIcon icon={faGithubAlt} />
+      <FontAwesomeIcon icon={faGoogle} />
+      <FontAwesomeIcon icon={faFacebook} />
+      <FontAwesomeIcon icon={faTwitter} />
+      <FontAwesomeIcon icon={["fas", "code"]} />
+      <FontAwesomeIcon icon={["fal", "code"]} />;
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
